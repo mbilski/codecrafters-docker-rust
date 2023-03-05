8d469b1052aa88a09688034eda002ae58962f204
Mateusz Bilski <mateusz.bilski@gmail.com>
2023-03-05 22:18:48 +0100

    Pipe stdin and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use std::process::Stdio;
+
 use anyhow::{Context, Result};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    let process = std::process::Command::new(command)
         .args(command_args)
-        .output()
+        .stdin(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .and_then(|mut s| s.wait())
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
@@ -18,10 +20,7 @@ fn main() -> Result<()> {
             )
         })?;
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
+    if !process.success() {
         std::process::exit(1);
     }
 
